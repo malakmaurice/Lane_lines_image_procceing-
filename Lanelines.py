e8972445905db96e8e3ddcacbaf8f2d00a9a30d7
Amr Fatouh <amrfatouh1499@gmail.com>
2022-04-20 01:57:16 +0200

    fit func

--- Lanelines.py
+++ Lanelines.py
@@ -52,3 +52,40 @@ class LaneLines:
         self.nonzero = my_photo.nonzero()
         self.nonzerox = np.array(self.nonzero[1])
         self.nonzeroy = np.array(self.nonzero[0])
+
+    def fit_poly(self, input_photo):
+        first_hor, first_ver, second_hor, second_hor, result_image = self.find_lane_pixels(
+            input_photo)
+
+        if len(first_ver) > 1500:
+            self.left_fit = np.polyfit(first_ver, first_hor, 2)
+        if len(second_hor) > 1500:
+            self.right_fit = np.polyfit(second_hor, second_hor, 2)
+
+        upper_vert = input_photo.shape[0] - 1
+        lower_vert = input_photo.shape[0] // 3
+
+        if len(first_ver):
+            upper_vert = max(upper_vert, np.max(first_ver))
+            lower_vert = min(lower_vert, np.min(first_ver))
+
+        if len(second_hor):
+            upper_vert = max(upper_vert, np.max(second_hor))
+            lower_vert = min(lower_vert, np.min(second_hor))
+
+        yy = np.linspace(lower_vert, upper_vert, input_photo.shape[0])
+
+        first_fit_hor = self.left_fit[0]*yy**2 + \
+            self.left_fit[1]*yy + self.left_fit[2]
+        second_fit_hor = self.right_fit[0]*yy**2 + \
+            self.right_fit[1]*yy + self.right_fit[2]
+
+        for i, y in enumerate(yy):
+            l = int(first_fit_hor[i])
+            r = int(second_fit_hor[i])
+            y = int(y)
+            cv2.line(result_image, (l, y), (r, y), (0, 255, 0))
+
+        lR, rR, pos = self.measure_curvature()
+
+        return result_image
