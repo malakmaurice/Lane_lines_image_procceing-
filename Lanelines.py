05d6d0d785b8c0077c6b3a25d0cd0bf0290c5e88
Amr Fatouh <amrfatouh1499@gmail.com>
2022-04-20 16:51:45 +0200

    changing some code

--- Lanelines.py
+++ Lanelines.py
@@ -1,13 +1,31 @@
 import cv2
-import matplotlib.image as mpimg
 import numpy as np
+import matplotlib.image as mpimg
+
 
 def hist(img):
-    bottom_half = img[img.shape[0]//2:,:]
+    bottom_half = img[img.shape[0]//2:, :]
     return np.sum(bottom_half, axis=0)
 
+
 class LaneLines:
+    """ Class containing information about detected lane lines.
+
+    Attributes:
+        left_fit (np.array): Coefficients of a polynomial that fit left lane line
+        right_fit (np.array): Coefficients of a polynomial that fit right lane line
+        parameters (dict): Dictionary containing all parameters needed for the pipeline
+        debug (boolean): Flag for debug/normal mode
+    """
+
     def __init__(self):
+        """Init Lanelines.
+
+        Parameters:
+            left_fit (np.array): Coefficients of polynomial that fit left lane
+            right_fit (np.array): Coefficients of polynomial that fit right lane
+            binary (np.array): binary image
+        """
         self.left_fit = None
         self.right_fit = None
         self.binary = None
@@ -15,90 +33,97 @@ class LaneLines:
         self.nonzerox = None
         self.nonzeroy = None
         self.clear_visibility = True
-
         self.dir = []
-
         self.left_curve_img = mpimg.imread('left_turn.png')
         self.right_curve_img = mpimg.imread('right_turn.png')
         self.keep_straight_img = mpimg.imread('straight.png')
-
         self.left_curve_img = cv2.normalize(
-            src=self.left_curve_img,
-            dst=None, alpha=0, beta=255,
-            norm_type=cv2.NORM_MINMAX,
-            dtype=cv2.CV_8U
-        )
-
+            src=self.left_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
         self.right_curve_img = cv2.normalize(
-            src=self.right_curve_img,
-            dst=None,
-            alpha=0,
-            beta=255,
-            norm_type=cv2.NORM_MINMAX,
-            dtype=cv2.CV_8U
-        )
-
+            src=self.right_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
         self.keep_straight_img = cv2.normalize(
-            src=self.keep_straight_img,
-            dst=None, alpha=0, beta=255,
-            norm_type=cv2.NORM_MINMAX,
-            dtype=cv2.CV_8U
-        )
+            src=self.keep_straight_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
+        # HYPERPARAMETERS
+        # Number of sliding windows
         self.nwindows = 9
+        # Width of the the windows +/- margin
         self.margin = 100
+        # Mininum number of pixels found to recenter window
         self.minpix = 50
 
-    # a method that extracts the feature of an image and store them in the class
-    def extract_features(self, my_photo):
-        self.img = my_photo
-        self.window_height = np.int(my_photo.shape[0]//self.nwindows)
-        self.nonzero = my_photo.nonzero()
+    def forward(self, img):
+        """Take a image and detect lane lines.
+
+        Parameters:
+            img (np.array): An binary image containing relevant pixels
+
+        Returns:
+            Image (np.array): An RGB image containing lane lines pixels and other details
+        """
+        self.extract_features(img)
+        return self.fit_poly(img)
+
+    def extract_features(self, img):
+        """ Extract features from a binary image
+
+        Parameters:
+            img (np.array): A binary image
+        """
+        self.img = img
+        # Height of of windows - based on nwindows and image shape
+        self.window_height = np.int(img.shape[0]//self.nwindows)
+
+        # Identify the x and y positions of all nonzero pixel in the image
+        self.nonzero = img.nonzero()
         self.nonzerox = np.array(self.nonzero[1])
         self.nonzeroy = np.array(self.nonzero[0])
 
-    # a method that finds the lane of an image
-    def fit_poly(self, input_photo):
-        first_hor, first_ver, second_hor, second_hor, result_image = self.find_lane_pixels(
-            input_photo)
+    def fit_poly(self, img):
+        """Find the lane line from an image and draw it.
 
-        if len(first_ver) > 1500:
-            self.left_fit = np.polyfit(first_ver, first_hor, 2)
-        if len(second_hor) > 1500:
-            self.right_fit = np.polyfit(second_hor, second_hor, 2)
+        Parameters:
+            img (np.array): a binary warped image
 
-        upper_vert = input_photo.shape[0] - 1
-        lower_vert = input_photo.shape[0] // 3
+        Returns:
+            out_img (np.array): a RGB image that have lane line drawn on that.
+        """
 
-        if len(first_ver):
-            upper_vert = max(upper_vert, np.max(first_ver))
-            lower_vert = min(lower_vert, np.min(first_ver))
+        leftx, lefty, rightx, righty, out_img = self.find_lane_pixels(img)
 
-        if len(second_hor):
-            upper_vert = max(upper_vert, np.max(second_hor))
-            lower_vert = min(lower_vert, np.min(second_hor))
+        if len(lefty) > 1500:
+            self.left_fit = np.polyfit(lefty, leftx, 2)
+        if len(righty) > 1500:
+            self.right_fit = np.polyfit(righty, rightx, 2)
 
-        yy = np.linspace(lower_vert, upper_vert, input_photo.shape[0])
+        # Generate x and y values for plotting
+        maxy = img.shape[0] - 1
+        miny = img.shape[0] // 3
+        if len(lefty):
+            maxy = max(maxy, np.max(lefty))
+            miny = min(miny, np.min(lefty))
 
-        first_fit_hor = self.left_fit[0]*yy**2 + \
-            self.left_fit[1]*yy + self.left_fit[2]
-        second_fit_hor = self.right_fit[0]*yy**2 + \
-            self.right_fit[1]*yy + self.right_fit[2]
+        if len(righty):
+            maxy = max(maxy, np.max(righty))
+            miny = min(miny, np.min(righty))
 
-        for i, y in enumerate(yy):
-            l = int(first_fit_hor[i])
-            r = int(second_fit_hor[i])
+        ploty = np.linspace(miny, maxy, img.shape[0])
+
+        left_fitx = self.left_fit[0]*ploty**2 + \
+            self.left_fit[1]*ploty + self.left_fit[2]
+        right_fitx = self.right_fit[0]*ploty**2 + \
+            self.right_fit[1]*ploty + self.right_fit[2]
+
+        # Visualization
+        for i, y in enumerate(ploty):
+            l = int(left_fitx[i])
+            r = int(right_fitx[i])
             y = int(y)
-            cv2.line(result_image, (l, y), (r, y), (0, 255, 0))
+            cv2.line(out_img, (l, y), (r, y), (0, 255, 0))
 
         lR, rR, pos = self.measure_curvature()
 
-        return result_image
-
-    # a method that detects the lanes of a given image through taking a binary image and returning an image with the lane pixels
-    def forward(self, img):
-        self.extract_features(img)
-        return self.fit_poly(img)
+        return out_img
 
     # return all pixels in a specific window
     # inputs:
@@ -108,14 +133,16 @@ class LaneLines:
     # outputs:
     #   (np.array): x coordinates of pixels that lie inside the window
     #   (np.array): y coordinates of pixels that lie inside the window
+
     def pixels_in_window(self, center, margin, height):
         top_left = (center[0]-margin, center[1]-height//2)
         bottom_right = (center[0]+margin, center[1]+height//2)
 
-        conditionx = (top_left[0] <= self.nonzerox) & (self.nonzerox <= bottom_right[0])
-        conditiony = (top_left[1] <= self.nonzeroy) & (self.nonzeroy <= bottom_right[1])
-        return self.nonzerox[conditionx&conditiony], self.nonzeroy[conditionx&conditiony]
-
+        conditionx = (top_left[0] <= self.nonzerox) & (
+            self.nonzerox <= bottom_right[0])
+        conditiony = (top_left[1] <= self.nonzeroy) & (
+            self.nonzeroy <= bottom_right[1])
+        return self.nonzerox[conditionx & conditiony], self.nonzeroy[conditionx & conditiony]
 
     # return lane pixels from a binary warped image
     # inputs:
@@ -126,6 +153,7 @@ class LaneLines:
     #   right_x (np.array): x coordinates of right lane pixels
     #   right_y (np.array): y coordinates of right lane pixels
     #   output_img (np.array): A RGB image that use to display result later on
+
     def find_lane_pixels(self, img):
         assert(len(img.shape) == 2)
 
@@ -147,8 +175,10 @@ class LaneLines:
             center_left = (left_x_current, y_current)
             center_right = (right_x_current, y_current)
 
-            good_left_x, good_left_y = self.pixels_in_window(center_left, self.margin, self.window_height)
-            good_right_x, good_right_y = self.pixels_in_window(center_right, self.margin, self.window_height)
+            good_left_x, good_left_y = self.pixels_in_window(
+                center_left, self.margin, self.window_height)
+            good_right_x, good_right_y = self.pixels_in_window(
+                center_right, self.margin, self.window_height)
 
             left_x.extend(good_left_x)
             left_y.extend(good_left_y)
@@ -161,6 +191,7 @@ class LaneLines:
                 right_x_current = np.int32(np.mean(good_right_x))
 
         return left_x, left_y, right_x, right_y, output_img
+
     def plot(self, out_img):
         np.set_printoptions(precision=6, suppress=True)
         lR, rR, pos = self.measure_curvature()
@@ -177,7 +208,7 @@ class LaneLines:
             self.dir.append('L')
         else:
             self.dir.append('R')
-        
+
         if len(self.dir) > 10:
             self.dir.pop(0)
 
@@ -185,30 +216,32 @@ class LaneLines:
         H = 500
         widget = np.copy(out_img[:H, :W])
         widget //= 2
-        widget[0,:] = [0, 0, 255]
-        widget[-1,:] = [0, 0, 255]
-        widget[:,0] = [0, 0, 255]
-        widget[:,-1] = [0, 0, 255]
+        widget[0, :] = [0, 0, 255]
+        widget[-1, :] = [0, 0, 255]
+        widget[:, 0] = [0, 0, 255]
+        widget[:, -1] = [0, 0, 255]
         out_img[:H, :W] = widget
 
-        direction = max(set(self.dir), key = self.dir.count)
+        direction = max(set(self.dir), key=self.dir.count)
         msg = "Keep Straight Ahead"
         curvature_msg = "Curvature = {:.0f} m".format(min(lR, rR))
         if direction == 'L':
-            y, x = self.left_curve_img[:,:,3].nonzero()
+            y, x = self.left_curve_img[:, :, 3].nonzero()
             out_img[y, x-100+W//2] = self.left_curve_img[y, x, :3]
             msg = "Left Curve Ahead"
         if direction == 'R':
-            y, x = self.right_curve_img[:,:,3].nonzero()
+            y, x = self.right_curve_img[:, :, 3].nonzero()
             out_img[y, x-100+W//2] = self.right_curve_img[y, x, :3]
             msg = "Right Curve Ahead"
         if direction == 'F':
-            y, x = self.keep_straight_img[:,:,3].nonzero()
+            y, x = self.keep_straight_img[:, :, 3].nonzero()
             out_img[y, x-100+W//2] = self.keep_straight_img[y, x, :3]
 
-        cv2.putText(out_img, msg, org=(10, 240), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+        cv2.putText(out_img, msg, org=(10, 240), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1, color=(255, 255, 255), thickness=2)
         if direction in 'LR':
-            cv2.putText(out_img, curvature_msg, org=(10, 280), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+            cv2.putText(out_img, curvature_msg, org=(
+                10, 280), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
 
         cv2.putText(
             out_img,
@@ -239,11 +272,12 @@ class LaneLines:
         y_eval = 700 * ym
 
         # Compute R_curve (radius of curvature)
-        left_curveR =  ((1 + (2*left_fit[0] *y_eval + left_fit[1])**2)**1.5)  / np.absolute(2*left_fit[0])
-        right_curveR = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+        left_curveR = (
+            (1 + (2*left_fit[0] * y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+        right_curveR = (
+            (1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
 
         xl = np.dot(self.left_fit, [700**2, 700, 1])
         xr = np.dot(self.right_fit, [700**2, 700, 1])
         pos = (1280//2 - (xl+xr)//2)*xm
-        return left_curveR, right_curveR, pos 
-    
+        return left_curveR, right_curveR, pos
